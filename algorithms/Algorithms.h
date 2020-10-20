43f8a74e693169b1449baf2c023bcb8e9457ff23
ekd <phekd94@gmail.com>
2020-10-20 09:34:32 +0300

    Delete unusing functions; some corrections

--- algorithms/Algorithms.h
+++ algorithms/Algorithms.h
@@ -4,11 +4,11 @@
 //-------------------------------------------------------------------------------------------------
 /*
 DESCRITION: Functions with algoritms
-TODO: * test class (logic() method)
+TODO:
+ * test class (logic() method)
 FIXME:
 DANGER:
-NOTE: * "const" in lambda function definition: "const" makes the linkage internal by default,
-		 which means it is only accessible in one translation unit, thereby avoiding the problem.
+NOTE:
 
 +---------------+------------+
 | thread safety | reentrance |
@@ -19,28 +19,12 @@ NOTE: * "const" in lambda function definition: "const" makes the linkage interna
 
 //-------------------------------------------------------------------------------------------------
 #include <cstdint>  // integer types
-#include <QString>  // QString class
 #include <vector>   // std::vector template class
 #include <cmath>    // std::exp(); DBL_MIN
 
 //-------------------------------------------------------------------------------------------------
 namespace Sec_145
 {
-
-//-------------------------------------------------------------------------------------------------
-// Converts pixel limits from [0, 255] to [0, 1]
-const auto convertPixelLimits = [](double pixel){ return pixel /= 255; };
-
-//-------------------------------------------------------------------------------------------------
-// Calculates a dot product
-double dotProduct(const double* const d_1, const double* const d_2, const uint32_t n);
-double dotProduct(const std::vector<double>& d_1, const std::vector<double>& d_2);
-
-//-------------------------------------------------------------------------------------------------
-// Calculates a correlation coefficient
-double correlationCoefficient(const uint8_t* const d_1, const uint8_t* const d_2,
-                              const uint32_t n);
-double correlationCoefficient(const std::vector<uint8_t>& d_1, const std::vector<uint8_t>& d_2);
 
 //-------------------------------------------------------------------------------------------------
 // Maximum array element
@@ -101,7 +85,7 @@ uint32_t minArrayElementIndex(Array* const array, const uint32_t size)
 }
 
 //-------------------------------------------------------------------------------------------------
-// Summary of array elements
+// Sum of array elements
 template <typename DataType>
 double sumArrayElements(DataType* const data, const uint32_t size)
 {
@@ -113,7 +97,7 @@ double sumArrayElements(DataType* const data, const uint32_t size)
 }
 
 //-------------------------------------------------------------------------------------------------
-// Summary of array elements
+// Sum of exponents of array elements
 template <typename DataType>
 double sumArrayExpElements(DataType* const data, const uint32_t size)
 {
